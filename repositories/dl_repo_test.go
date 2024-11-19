54b48718a1cb7a95fc25cf222f4c07b0690dfc60
OMiD-Borzoei <omid.7968.june@gmail.com>
2024-11-19 17:09:37 +0330

    uncommented some lines

--- repositories/dl_repo_test.go
+++ repositories/dl_repo_test.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
 	"gorm.io/gorm"
 )
 
@@ -82,317 +83,313 @@ func TestAllDLmethods(t *testing.T) {
 
 	repo.db.AutoMigrate(&models.DL{})
 
-	//var code, title string
+	var code, title string
 
-	// t.Run("DL CRUD", func(t *testing.T) {
-	// 	t.Run("Create", func(t *testing.T) {
+	t.Run("DL CRUD", func(t *testing.T) {
+		t.Run("Create", func(t *testing.T) {
 
-	// 		t.Run("Should not Add DL with empty code", func(t *testing.T) {
-	// 			code, title = "", fmt.Sprintf("%d", time.Now().UnixNano())
-	// 			_, err := repo.Create(code, title)
-	// 			assert.NotNil(t, err)
-	// 		})
+			t.Run("Should not Add DL with empty code", func(t *testing.T) {
+				code, title = "", fmt.Sprintf("%d", time.Now().UnixNano())
+				_, err := repo.Create(code, title)
+				assert.NotNil(t, err)
+			})
 
-	// 		t.Run("Should not Add DL with empty title", func(t *testing.T) {
-	// 			code, title = fmt.Sprintf("%d", time.Now().UnixNano()), ""
-	// 			_, err := repo.Create(code, title)
-	// 			assert.NotNil(t, err)
-	// 		})
+			t.Run("Should not Add DL with empty title", func(t *testing.T) {
+				code, title = fmt.Sprintf("%d", time.Now().UnixNano()), ""
+				_, err := repo.Create(code, title)
+				assert.NotNil(t, err)
+			})
 
-	// 		t.Run("Should not Add DL with a code that has more that 64 chars", func(t *testing.T) {
-	// 			code, title = fmt.Sprintf("%d", time.Now().UnixNano()), "acceptable title"
-	// 			for range 65 {
-	// 				code += "a"
-	// 			}
-	// 			_, err := repo.Create(code, title)
-	// 			assert.NotNil(t, err)
-	// 		})
+			t.Run("Should not Add DL with a code that has more that 64 chars", func(t *testing.T) {
+				code, title = fmt.Sprintf("%d", time.Now().UnixNano()), "acceptable title"
+				for range 65 {
+					code += "a"
+				}
+				_, err := repo.Create(code, title)
+				assert.NotNil(t, err)
+			})
 
-	// 		t.Run("Should not Add DL with a title that has more that 64 chars", func(t *testing.T) {
-	// 			code, title = "acceptable code", fmt.Sprintf("%d", time.Now().UnixNano())
-	// 			for range 65 {
-	// 				title += "a"
-	// 			}
-	// 			_, err := repo.Create(code, title)
-	// 			assert.NotNil(t, err)
-	// 		})
+			t.Run("Should not Add DL with a title that has more that 64 chars", func(t *testing.T) {
+				code, title = "acceptable code", fmt.Sprintf("%d", time.Now().UnixNano())
+				for range 65 {
+					title += "a"
+				}
+				_, err := repo.Create(code, title)
+				assert.NotNil(t, err)
+			})
 
-	// 		t.Run("Should be Able to add a DL with code that has 64 persian chars", func(t *testing.T) {
-	// 			code, title = stamp("")
-	// 			for range 64 - len(code) {
-	// 				code += "م"
-	// 			}
-	// 			_, err := repo.Create(code, title)
-	// 			assert.Nil(t, err)
+			t.Run("Should be Able to add a DL with code that has 64 persian chars", func(t *testing.T) {
+				code, title = stamp("")
+				for range 64 - len(code) {
+					code += "م"
+				}
+				_, err := repo.Create(code, title)
+				assert.Nil(t, err)
 
-	// 			// Check for correct insertion in database
-	// 			dl := &models.DL{}
-	// 			repo.db.Model(&models.DL{}).Last(dl)
-	// 			assert.Equal(t, dl.Code, code)
-	// 			assert.Equal(t, dl.Title, title)
-	// 		})
+				// Check for correct insertion in database
+				dl := &models.DL{}
+				repo.db.Model(&models.DL{}).Last(dl)
+				assert.Equal(t, dl.Code, code)
+				assert.Equal(t, dl.Title, title)
+			})
 
-	// 		t.Run("Should be Able to add a DL with title that has 64 persian chars", func(t *testing.T) {
-	// 			code, title = stamp("")
-	// 			for range 64 - len(title) {
-	// 				title += "م"
-	// 			}
-	// 			_, err := repo.Create(code, title)
-	// 			assert.Nil(t, err)
+			t.Run("Should be Able to add a DL with title that has 64 persian chars", func(t *testing.T) {
+				code, title = stamp("")
+				for range 64 - len(title) {
+					title += "م"
+				}
+				_, err := repo.Create(code, title)
+				assert.Nil(t, err)
 
-	// 			// Check for correct insertion in database
-	// 			dl := &models.DL{}
-	// 			repo.db.Model(&models.DL{}).Last(dl)
-	// 			assert.Equal(t, dl.Code, code)
-	// 			assert.Equal(t, dl.Title, title)
+				// Check for correct insertion in database
+				dl := &models.DL{}
+				repo.db.Model(&models.DL{}).Last(dl)
+				assert.Equal(t, dl.Code, code)
+				assert.Equal(t, dl.Title, title)
 
-	// 		})
+			})
 
-	// 		t.Run("Shouldn't Add a DL with repetative code", func(t *testing.T) {
-	// 			code, title = stamp("D001")
-	// 			_, err := repo.Create(code, title)
-	// 			assert.Nil(t, err)
+			t.Run("Shouldn't Add a DL with repetative code", func(t *testing.T) {
+				code, title = stamp("D001")
+				_, err := repo.Create(code, title)
+				assert.Nil(t, err)
 
-	// 			_, err = repo.Create(code, title+"new")
-	// 			assert.NotNil(t, err)
-	// 		})
+				_, err = repo.Create(code, title+"new")
+				assert.NotNil(t, err)
+			})
 
-	// 		t.Run("Shouldn't Add a DL with repetative title", func(t *testing.T) {
-	// 			code, title = stamp("D002")
-	// 			_, err := repo.Create(code, title)
-	// 			assert.Nil(t, err)
+			t.Run("Shouldn't Add a DL with repetative title", func(t *testing.T) {
+				code, title = stamp("D002")
+				_, err := repo.Create(code, title)
+				assert.Nil(t, err)
 
-	// 			_, err = repo.Create(code+"new", title)
-	// 			assert.NotNil(t, err)
-	// 		})
+				_, err = repo.Create(code+"new", title)
+				assert.NotNil(t, err)
+			})
 
-	// 	})
+		})
 
-	// 	t.Run("Read", func(t *testing.T) {
-	// 		dl := &models.DL{}
-	// 		if err := repo.db.Model(&models.DL{}).First(dl).Error; err != nil {
-	// 			fmt.Println("DB issue, aborting...")
-	// 			return
-	// 		}
+		t.Run("Read", func(t *testing.T) {
+			dl := &models.DL{}
+			if err := repo.db.Model(&models.DL{}).First(dl).Error; err != nil {
+				fmt.Println("DB issue, aborting...")
+				return
+			}
 
-	// 		readdl, err := repo.Read(dl.ID)
-	// 		assert.Nil(t, err)
-	// 		assert.Equal(t, dl, readdl)
-	// 	})
+			readdl, err := repo.Read(dl.ID)
+			assert.Nil(t, err)
+			assert.Equal(t, dl, readdl)
+		})
 
-	// 	t.Run("Update", func(t *testing.T) {
-	// 		dl := &models.DL{}
-	// 		if err := repo.db.Model(&models.DL{}).Last(dl).Error; err != nil {
-	// 			fmt.Println("DB issue, aborting...")
-	// 			return
-	// 		}
+		t.Run("Update", func(t *testing.T) {
+			dl := &models.DL{}
+			if err := repo.db.Model(&models.DL{}).Last(dl).Error; err != nil {
+				fmt.Println("DB issue, aborting...")
+				return
+			}
 
-	// 		t.Run("Shouldnt Update DL Code to empty string", func(t *testing.T) {
-	// 			newdl := &models.DL{
-	// 				Code:  "",
-	// 				Title: "Valid" + fmt.Sprintf("%d", time.Now().UnixNano()),
-	// 			}
+			t.Run("Shouldnt Update DL Code to empty string", func(t *testing.T) {
+				newdl := &models.DL{
+					Code:  "",
+					Title: "Valid" + fmt.Sprintf("%d", time.Now().UnixNano()),
+				}
 
-	// 			err := repo.Update(dl.ID, newdl)
-	// 			assert.NotNil(t, err)
-	// 		})
+				err := repo.Update(dl.ID, newdl)
+				assert.NotNil(t, err)
+			})
 
-	// 		t.Run("Shouldnt Update DL Title to empty string", func(t *testing.T) {
-	// 			newdl := &models.DL{
-	// 				Code:  "Valid" + fmt.Sprintf("%d", time.Now().UnixNano()),
-	// 				Title: "",
-	// 			}
+			t.Run("Shouldnt Update DL Title to empty string", func(t *testing.T) {
+				newdl := &models.DL{
+					Code:  "Valid" + fmt.Sprintf("%d", time.Now().UnixNano()),
+					Title: "",
+				}
 
-	// 			err := repo.Update(dl.ID, newdl)
-	// 			assert.NotNil(t, err)
-	// 		})
+				err := repo.Update(dl.ID, newdl)
+				assert.NotNil(t, err)
+			})
 
-	// 		t.Run("Shouldnt Update DL code to string with more than 64 chars", func(t *testing.T) {
-	// 			code, title = stamp("")
-	// 			for range 65 - len(code) {
-	// 				code += "o"
-	// 			}
-	// 			newdl := &models.DL{
-	// 				Code:  code,
-	// 				Title: title,
-	// 			}
+			t.Run("Shouldnt Update DL code to string with more than 64 chars", func(t *testing.T) {
+				code, title = stamp("")
+				for range 65 - len(code) {
+					code += "o"
+				}
+				newdl := &models.DL{
+					Code:  code,
+					Title: title,
+				}
 
-	// 			err := repo.Update(dl.ID, newdl)
-	// 			assert.NotNil(t, err)
-	// 		})
+				err := repo.Update(dl.ID, newdl)
+				assert.NotNil(t, err)
+			})
 
-	// 		t.Run("Shouldnt Update DL title to string with more than 64 chars", func(t *testing.T) {
-	// 			code, title = stamp("")
-	// 			for range 65 - len(title) {
-	// 				title += "o"
-	// 			}
-	// 			newdl := &models.DL{
-	// 				Code:  code,
-	// 				Title: title,
-	// 			}
+			t.Run("Shouldnt Update DL title to string with more than 64 chars", func(t *testing.T) {
+				code, title = stamp("")
+				for range 65 - len(title) {
+					title += "o"
+				}
+				newdl := &models.DL{
+					Code:  code,
+					Title: title,
+				}
 
-	// 			err := repo.Update(dl.ID, newdl)
-	// 			assert.NotNil(t, err)
-	// 		})
+				err := repo.Update(dl.ID, newdl)
+				assert.NotNil(t, err)
+			})
 
-	// 		t.Run("Shouldnt Update DL code to sth repetetive", func(t *testing.T) {
-	// 			firstdl := &models.DL{}
-	// 			repo.db.Model(&models.DL{}).First(firstdl)
+			t.Run("Shouldnt Update DL code to sth repetetive", func(t *testing.T) {
+				firstdl := &models.DL{}
+				repo.db.Model(&models.DL{}).First(firstdl)
 
-	// 			newdl := &models.DL{
-	// 				Code:  firstdl.Code,
-	// 				Title: "sth valid",
-	// 			}
+				newdl := &models.DL{
+					Code:  firstdl.Code,
+					Title: "sth valid",
+				}
 
-	// 			err := repo.Update(dl.ID, newdl)
-	// 			assert.NotNil(t, err)
-	// 		})
+				err := repo.Update(dl.ID, newdl)
+				assert.NotNil(t, err)
+			})
 
-	// 		t.Run("Shouldnt Update DL titel to sth repetetive", func(t *testing.T) {
-	// 			firstdl := &models.DL{}
-	// 			repo.db.Model(&models.DL{}).First(firstdl)
+			t.Run("Shouldnt Update DL titel to sth repetetive", func(t *testing.T) {
+				firstdl := &models.DL{}
+				repo.db.Model(&models.DL{}).First(firstdl)
 
-	// 			newdl := &models.DL{
-	// 				Code:  "sth valid",
-	// 				Title: firstdl.Title,
-	// 			}
+				newdl := &models.DL{
+					Code:  "sth valid",
+					Title: firstdl.Title,
+				}
 
-	// 			err := repo.Update(dl.ID, newdl)
-	// 			assert.NotNil(t, err)
-	// 		})
+				err := repo.Update(dl.ID, newdl)
+				assert.NotNil(t, err)
+			})
 
-	// 		t.Run("should update to a valid state", func(t *testing.T) {
-	// 			code, title = stamp("D003-Updated")
-	// 			newdl := &models.DL{
-	// 				Code:  code,
-	// 				Title: title,
-	// 			}
-	// 			err := repo.Update(dl.ID, newdl)
-	// 			assert.Nil(t, err)
+			t.Run("should update to a valid state", func(t *testing.T) {
+				code, title = stamp("D003-Updated")
+				newdl := &models.DL{
+					Code:  code,
+					Title: title,
+				}
+				err := repo.Update(dl.ID, newdl)
+				assert.Nil(t, err)
 
-	// 			updateddl := &models.DL{}
-	// 			repo.db.Model(&models.DL{}).First(updateddl, "id = ?", dl.ID)
+				updateddl := &models.DL{}
+				repo.db.Model(&models.DL{}).First(updateddl, "id = ?", dl.ID)
 
-	// 			assert.Equal(t, code, updateddl.Code)
-	// 			assert.Equal(t, title, updateddl.Title)
+				assert.Equal(t, code, updateddl.Code)
+				assert.Equal(t, title, updateddl.Title)
 
-	// 		})
+			})
 
-	// 		t.Run("should update a DL that is referenced", func(t *testing.T) {
-	// 			sl1_id, sl2_id, dl_id, v_id := scenario1(repo.db)
+			t.Run("should update a DL that is referenced", func(t *testing.T) {
+				sl1_id, sl2_id, dl_id, v_id := scenario1(repo.db)
 
-	// 			code, title = stamp("")
-	// 			validdl := &models.DL{
-	// 				Code:  code,
-	// 				Title: title,
-	// 			}
+				code, title = stamp("")
+				validdl := &models.DL{
+					Code:  code,
+					Title: title,
+				}
 
-	// 			assert.Nil(t, repo.Update(dl_id, validdl))
+				assert.Nil(t, repo.Update(dl_id, validdl))
 
-	// 			dl, _ := repo.Read(dl_id)
-	// 			assert.Equal(t, dl.Version, uint(1))
+				dl, _ := repo.Read(dl_id)
+				assert.Equal(t, dl.Version, uint(1))
 
-	// 			revert_scenario1(repo.db, sl1_id, sl2_id, dl_id, v_id)
+				revert_scenario1(repo.db, sl1_id, sl2_id, dl_id, v_id)
 
-	// 		})
+			})
 
-	// 		t.Run("Row Versioning", func(t *testing.T) {
-	// 			code, title = stamp("D003-Updated-Twice")
-	// 			newdl := &models.DL{
-	// 				Code:  code,
-	// 				Title: title,
-	// 			}
-	// 			err := repo.Update(dl.ID, newdl)
-	// 			assert.NotNil(t, err)
+			t.Run("Row Versioning", func(t *testing.T) {
+				code, title = stamp("D003-Updated-Twice")
+				newdl := &models.DL{
+					Code:  code,
+					Title: title,
+				}
+				err := repo.Update(dl.ID, newdl)
+				assert.NotNil(t, err)
 
-	// 			newdl.Version = 1
-	// 			err = repo.Update(dl.ID, newdl)
-	// 			assert.Nil(t, err)
+				newdl.Version = 1
+				err = repo.Update(dl.ID, newdl)
+				assert.Nil(t, err)
 
-	// 			code, title = stamp("D003-Updated-Triple")
-	// 			newdl = &models.DL{
-	// 				Code:  code,
-	// 				Title: title,
-	// 			}
-	// 			err = repo.Update(dl.ID, newdl)
-	// 			assert.NotNil(t, err)
+				code, title = stamp("D003-Updated-Triple")
+				newdl = &models.DL{
+					Code:  code,
+					Title: title,
+				}
+				err = repo.Update(dl.ID, newdl)
+				assert.NotNil(t, err)
 
-	// 			newdl.Version = 2
-	// 			err = repo.Update(dl.ID, newdl)
-	// 			assert.Nil(t, err)
+				newdl.Version = 2
+				err = repo.Update(dl.ID, newdl)
+				assert.Nil(t, err)
 
-	// 			code, title = stamp("D003-Updated-4th")
-	// 			newdl = &models.DL{
-	// 				Code:  code,
-	// 				Title: title,
-	// 			}
-	// 			err = repo.Update(dl.ID, newdl)
-	// 			assert.NotNil(t, err)
+				code, title = stamp("D003-Updated-4th")
+				newdl = &models.DL{
+					Code:  code,
+					Title: title,
+				}
+				err = repo.Update(dl.ID, newdl)
+				assert.NotNil(t, err)
 
-	// 		})
+			})
 
-	// 	})
+		})
 
-	// 	t.Run("Delete", func(t *testing.T) {
+		t.Run("Delete", func(t *testing.T) {
 
-	// 		t.Run("Should Delete the last 4 DL's", func(t *testing.T) {
+			t.Run("Should Delete the last 4 DL's", func(t *testing.T) {
 
-	// 			DL := &models.DL{}
-	// 			if err := repo.db.Model(&models.DL{}).Last(DL).Error; err != nil {
-	// 				fmt.Println("DB issue, aborting...", err)
-	// 				return
-	// 			}
-	// 			err := repo.Delete(DL.ID)
-	// 			assert.Nil(t, err)
-	// 			_, err = repo.Read(DL.ID)
-	// 			assert.NotNil(t, err)
+				DL := &models.DL{}
+				if err := repo.db.Model(&models.DL{}).Last(DL).Error; err != nil {
+					fmt.Println("DB issue, aborting...", err)
+					return
+				}
+				err := repo.Delete(DL.ID)
+				assert.Nil(t, err)
+				_, err = repo.Read(DL.ID)
+				assert.NotNil(t, err)
 
-	// 			DL = &models.DL{}
-	// 			if err := repo.db.Model(&models.DL{}).Last(DL).Error; err != nil {
-	// 				fmt.Println("DB issue, aborting...", err)
-	// 				return
-	// 			}
+				DL = &models.DL{}
+				if err := repo.db.Model(&models.DL{}).Last(DL).Error; err != nil {
+					fmt.Println("DB issue, aborting...", err)
+					return
+				}
 
-	// 			err = repo.Delete(DL.ID)
-	// 			assert.Nil(t, err)
-	// 			_, err = repo.Read(DL.ID)
-	// 			assert.NotNil(t, err)
+				err = repo.Delete(DL.ID)
+				assert.Nil(t, err)
+				_, err = repo.Read(DL.ID)
+				assert.NotNil(t, err)
 
-	// 			DL = &models.DL{}
-	// 			if err := repo.db.Model(&models.DL{}).Last(DL).Error; err != nil {
-	// 				fmt.Println("DB issue, aborting...", err)
-	// 				return
-	// 			}
+				DL = &models.DL{}
+				if err := repo.db.Model(&models.DL{}).Last(DL).Error; err != nil {
+					fmt.Println("DB issue, aborting...", err)
+					return
+				}
 
-	// 			err = repo.Delete(DL.ID)
-	// 			assert.Nil(t, err)
-	// 			_, err = repo.Read(DL.ID)
-	// 			assert.NotNil(t, err)
+				err = repo.Delete(DL.ID)
+				assert.Nil(t, err)
+				_, err = repo.Read(DL.ID)
+				assert.NotNil(t, err)
 
-	// 			DL = &models.DL{}
-	// 			if err := repo.db.Model(&models.DL{}).Last(DL).Error; err != nil {
-	// 				fmt.Println("DB issue, aborting...", err)
-	// 				return
-	// 			}
+				DL = &models.DL{}
+				if err := repo.db.Model(&models.DL{}).Last(DL).Error; err != nil {
+					fmt.Println("DB issue, aborting...", err)
+					return
+				}
 
-	// 			err = repo.Delete(DL.ID)
-	// 			assert.Nil(t, err)
-	// 			_, err = repo.Read(DL.ID)
-	// 			assert.NotNil(t, err)
-	// 		})
+				err = repo.Delete(DL.ID)
+				assert.Nil(t, err)
+				_, err = repo.Read(DL.ID)
+				assert.NotNil(t, err)
+			})
 
-	// 		t.Run("Shouldnt Delete a DL that is referenced by a vi", func(t *testing.T) {
-	// 			sl1_id, sl2_id, dl_id, v_id := scenario1(repo.db)
+			t.Run("Shouldnt Delete a DL that is referenced by a vi", func(t *testing.T) {
+				sl1_id, sl2_id, dl_id, v_id := scenario1(repo.db)
 
-	// 			assert.NotNil(t, repo.Delete(dl_id))
+				assert.NotNil(t, repo.Delete(dl_id))
 
-	// 			revert_scenario1(repo.db, sl1_id, sl2_id, dl_id, v_id)
-	// 		})
-	// 	})
-	// })
-
-	t.Run("s", func(t *testing.T) {
-		scenario1(repo.db)
-		//revert_scenario1(repo.db, 91, 92, 85, 76)
+				revert_scenario1(repo.db, sl1_id, sl2_id, dl_id, v_id)
+			})
+		})
 	})
+
 }
